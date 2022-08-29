55ce8b968f22b95ab1af703f1091967731867f9a
v08nike <teddan817@gmail.com>
2022-08-28 21:53:49 -0300

    new update gitignore

--- dist/src/interfaces/indication.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// "indications":[
-//     {
-//       "person_code": 2,
-//       "product_name": "X"
-//       "dtBuy": "2022-08-01 10:55",
-//       "indication_code": "1a2b3c"
-//     },
--- dist/src/routes/users.routes.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// import { Router } from 'express';
-// const router = Router();
-// // router.post('/buyses', buysController.create);
-// export default router;
